4f0304190aed2fd8f460d4eab26d32bd2afe5d3a
gronkomatic <sam@gillham.xyz>
2024-03-14 21:45:22 +1100

    Update dataset_size to 500

--- small_mistral_hyperparameter_search_sfttrainer.py
+++ small_mistral_hyperparameter_search_sfttrainer.py
@@ -42,7 +42,7 @@ context_length = 2048  # Maximum sequence length
 dataset_name = "wikimedia/wikipedia"  # Name of the dataset to use
 dataset_config = "20231101.en"  # Configuration of the dataset to use
 dataset_path = "/media/gronkomatic/Embiggen/ai-stuff/datasets/wikipedia"  # Path to the dataset
-dataset_size = 250  # Number of examples to use from the dataset
+dataset_size = 500  # Number of examples to use from the dataset
 dataset_split = 0.9  # Percentage of examples to use for training
 stride = 50  # Stride for splitting the input into multiple sequences. Doesn't work with Mistral according to CoPilot, but what would they know?
 
