46c7c789701fc91d03aacb0372085e2ff5f0f1c3
gronkomatic <sam@gillham.xyz>
2024-04-06 21:47:29 +1100

    Setup small_mistral_hyperparameter_search_sfttrainer.py for lr search with 10000 examples

--- small_mistral_hyperparameter_search_sfttrainer.py
+++ small_mistral_hyperparameter_search_sfttrainer.py
@@ -60,29 +60,29 @@ hf_token = os.getenv("HF_TOKEN")
 template_model_name = "mistralai/Mistral-7B-v0.1"
 
 # Model settings - Model size: approx 420M parameters
-hidden_layers = 1  # Number of transformer layers
+hidden_layers = 2  # Number of transformer layers
 hidden_size = 1024  # Size of the hidden states in the transformer layers
 intermediate_size = 2048  # Size of the feed-forward network in the transformer layers
-attention_heads = 16  # Number of attention heads
-attn_dropout = 0.1  # Dropout rate for the attention probabilities
+attention_heads = 32  # Number of attention heads
+attn_dropout = 0.18486156277928165  # Dropout rate for the attention probabilities
 context_length = 512  # Maximum sequence length
 
 # Dataset settings
 dataset_name = "wikimedia/wikipedia"  # Name of the dataset to use
 dataset_config = "20231101.en"  # Configuration of the dataset to use
 dataset_path = "/media/gronkomatic/Embiggen/ai-stuff/datasets/wikipedia"  # Path to the dataset
-dataset_size = 1000  # Number of examples to use from the dataset
+dataset_size = 10000  # Number of examples to use from the dataset
 dataset_split = 0.9  # Percentage of examples to use for training
 stride = 50  # Stride for splitting the input into multiple sequences. Doesn't work with Mistral according to CoPilot, but what would they know?
 
 # Training settings
 seed = 42  # Random seed for reproducibility
 learning_rate = 1.2e-3  # Learning rate for the AdamW optimizer
-lr_scheduler_type = "linear"  # Use a cosine annealing learning rate scheduler
-num_train_epochs = 1  # Number of training epochs
-per_device_train_batch_size = 24  # Batch size per GPU/TPU core/CPU for training
+lr_scheduler_type = "polynomial"  # Use a cosine annealing learning rate scheduler
+num_train_epochs = 5  # Number of training epochs
+per_device_train_batch_size = 12  # Batch size per GPU/TPU core/CPU for training
 warmup_ratio = 0.10  # Ratio of the number of warmup steps to the total number of training steps
-weight_decay = 0.01  # Weight decay for the AdamW optimizer
+weight_decay = 0.06388269955610547  # Weight decay for the AdamW optimizer
 max_grad_norm = 1.0  # Maximum gradient norm
 gradient_accumulation_steps = 1  # Number of steps to accumulate gradients for
 gradient_checkpointing = False  # Causes a segfault when enabled
@@ -169,158 +169,157 @@ class Objective(TrainerCallback):
             os.makedirs(self.study_dir)
 
         self.best_loss = np.inf
-        self.best_perplexity = np.inf
 
     def __call__(self, trial: optuna.Trial) -> float:
-        # Model settings search space
-        attention_heads = trial.suggest_categorical("attention_heads", attention_heads_categorical)
+        try:
+            # Model settings search space
+            # attention_heads = trial.suggest_categorical("attention_heads", attention_heads_categorical)
 
-        # Hyperparameter search space
-        # learning_rate = trial.suggest_float("learning_rate", lr_range[0], lr_range[1])
-        # dataset_size = trial.suggest_categorical("dataset_size", dataset_size_categorical)
-        lr_scheduler_type = trial.suggest_categorical("lr_scheduler_type", lr_scheduler_types)
-        num_train_epochs = trial.suggest_int("num_train_epochs", train_epochs_range[0], train_epochs_range[1])
-        gradient_accumulation_steps = trial.suggest_categorical("gradient_accumulation_steps", gradient_accumulation_steps_categorical)
-        attn_dropout = trial.suggest_float("attn_dropout", attn_dropout_range[0], attn_dropout_range[1])
-        weight_decay = trial.suggest_float("weight_decay", weight_decay_range[0], weight_decay_range[1])
-        # max_grad_norm = trial.suggest_float("max_grad_norm", max_grad_norm_range[0], max_grad_norm_range[1])
+            # Hyperparameter search space
+            learning_rate = trial.suggest_float("learning_rate", lr_range[0], lr_range[1])
+            # dataset_size = trial.suggest_categorical("dataset_size", dataset_size_categorical)
+            # lr_scheduler_type = trial.suggest_categorical("lr_scheduler_type", lr_scheduler_types)
+            # num_train_epochs = trial.suggest_int("num_train_epochs", train_epochs_range[0], train_epochs_range[1])
+            # gradient_accumulation_steps = trial.suggest_categorical("gradient_accumulation_steps", gradient_accumulation_steps_categorical)
+            # attn_dropout = trial.suggest_float("attn_dropout", attn_dropout_range[0], attn_dropout_range[1])
+            # weight_decay = trial.suggest_float("weight_decay", weight_decay_range[0], weight_decay_range[1])
+            # max_grad_norm = trial.suggest_float("max_grad_norm", max_grad_norm_range[0], max_grad_norm_range[1])
 
-        # per_device_train_batch_size = trial.suggest_int("per_device_train_batch_size", per_device_train_batch_size_range[0], per_device_train_batch_size_range[1])
-        # warmup_ratio = trial.suggest_float("warmup_ratio", warmup_ratio_range[0], warmup_ratio_range[1])
+            # per_device_train_batch_size = trial.suggest_int("per_device_train_batch_size", per_device_train_batch_size_range[0], per_device_train_batch_size_range[1])
+            # warmup_ratio = trial.suggest_float("warmup_ratio", warmup_ratio_range[0], warmup_ratio_range[1])
 
-        # Reset the best loss
-        self.best_loss = np.inf
+            # Reset the best loss
+            self.best_loss = np.inf
 
-        # Reset the perplexity
-        self.best_perplexity = np.inf
+            # Reset the perplexity
+            self.best_perplexity = np.inf
 
-        results_dir = f"{self.study_dir}/optuna_trial_{trial.number}"
-        if not os.path.exists(results_dir):
-            os.makedirs(results_dir)
+            results_dir = f"{self.study_dir}/optuna_trial_{trial.number}"
+            if not os.path.exists(results_dir):
+                os.makedirs(results_dir)
 
-        # TrainingArguments setup
-        self.training_args = TrainingArguments(
-            output_dir=results_dir,
-            num_train_epochs=num_train_epochs,
-            per_device_train_batch_size=per_device_train_batch_size,
-            per_device_eval_batch_size=per_device_train_batch_size,
-            gradient_accumulation_steps=gradient_accumulation_steps,
-            gradient_checkpointing=gradient_checkpointing,
-            max_grad_norm=max_grad_norm,
-            warmup_ratio=warmup_ratio,
-            learning_rate=learning_rate,
-            lr_scheduler_type=lr_scheduler_type,
-            optim=optim,
-            weight_decay=weight_decay,
-            evaluation_strategy="epoch",
-            # eval_steps=0.2 / num_train_epochs - 0.001,
-            save_strategy="no",
-            # save_steps=0.5 / num_train_epochs - 0.001,
-            logging_dir=f"{results_dir}/logs/",
-            logging_strategy="steps",
-            logging_steps=0.1 / num_train_epochs,
-            report_to="none",
-            # load_best_model_at_end=True,
-            bf16=True,  # Enable mixed-precision training
-            bf16_full_eval=True,  # Enable mixed-precision evaluation
-            seed=seed,
-        )
-
-        # Prepare the dataset
-        self.prepare_dataset(dataset_size, dataset_split)
-
-        # Set the dropout rate and attention heads
-        config_1B.attention_dropout = attn_dropout
-        config_1B.num_attention_heads = attention_heads
-
-        # Initialize the trainer
-        trainer = SFTTrainer(
-            model_init=self.model_init,
-            args=self.training_args,
-            train_dataset=self.dataset_train,
-            eval_dataset=self.dataset_eval,
-            dataset_text_field="text",
-            packing=True,
-            max_seq_length=context_length,
-            tokenizer=tokenizer,
-            callbacks=[self],
-            # callbacks=[self, OptunaPruningCallback(trial, monitor="eval_loss")],
-        )
-
-        # Print the model size with suffix 'G' or 'M'
-        model_size = sum(p.numel() for p in trainer.model.parameters())
-        model_size = model_size / 1e9 if model_size > 1e9 else model_size / 1e6
-        model_size_suffix = "G" if model_size > 1e3 else "M"
-
-        dataset_train_size = len(self.dataset_train)
-        dataset_eval_size = len(self.dataset_eval)
-
-        # Print the hyperparameters
-        print("Hyperparameters:")
-        print(f"  Model size: {model_size:.2f}{model_size_suffix} parameters")
-        print(f"  Hidden layers: {hidden_layers}")
-        print(f"  Hidden size: {hidden_size}")
-        print(f"  Intermediate size: {intermediate_size}")
-        print(f"  Attention heads: {attention_heads}")
-        print(f"  Attention dropout: {attn_dropout}")
-        print(f"  Learning rate: {learning_rate}")
-        print(f"  Learning rate scheduler type: {lr_scheduler_type}")
-        print(f"  Epochs: {num_train_epochs}")
-        print(f"  Warmup ratio: {warmup_ratio}")
-        print(f"  Weight decay: {weight_decay}")
-        print(f"  Max gradient norm: {max_grad_norm}")
-        print(f"  Gradient accumulation steps: {gradient_accumulation_steps}")
-        print(f"  Per device train batch size: {per_device_train_batch_size}")
-        device_count = torch.cuda.device_count() if torch.cuda.is_available() else 1
-        print(f"  Device count: {device_count}")
-        print(f"  Effective batch size: {per_device_train_batch_size * gradient_accumulation_steps * device_count}")
-        print(f"  Dataset size: {dataset_size}")
-        print(f"  Training set size: {dataset_train_size}")
-        print(f"  Evaluation set size: {dataset_eval_size}")
-
-        # Save all the details to a JSON file in the results directory
-        with open(f"{results_dir}/details.json", "w") as f:
-            json.dump(
-                {
-                    "model_size": f"{model_size:.2f}{model_size_suffix}",
-                    "learning_rate": learning_rate,
-                    "lr_scheduler_type": lr_scheduler_type,
-                    "per_device_train_batch_size": per_device_train_batch_size,
-                    "epochs": num_train_epochs,
-                    "warmup_ratio": warmup_ratio,
-                    "attention_dropout": attn_dropout,
-                    "gradient_accumulation_steps": gradient_accumulation_steps,
-                    "dataset_train_size": dataset_train_size,
-                    "dataset_eval_size": dataset_eval_size,
-                    # Model settings
-                    "hidden_layers": hidden_layers,
-                    "hidden_size": hidden_size,
-                    "intermediate_size": intermediate_size,
-                    "attention_heads": attention_heads,
-                    "attn_dropout": attn_dropout,
-                    "context_length": context_length,
-                    "template_model_name": template_model_name,
-                    # Dataset settings
-                    "dataset_name": dataset_name,
-                    "dataset_config": dataset_config,
-                    "dataset_path": dataset_path,
-                    "dataset_size": dataset_size,
-                    "dataset_split": dataset_split,
-                    "stride": stride,
-                    # Training settings
-                    "seed": seed,
-                    "lr_range": lr_range,
-                    "lr_scheduler_type": lr_scheduler_type,
-                    # "optim": optim,
-                },
-                f,
+            # TrainingArguments setup
+            self.training_args = TrainingArguments(
+                output_dir=results_dir,
+                num_train_epochs=num_train_epochs,
+                per_device_train_batch_size=per_device_train_batch_size,
+                per_device_eval_batch_size=per_device_train_batch_size,
+                gradient_accumulation_steps=gradient_accumulation_steps,
+                gradient_checkpointing=gradient_checkpointing,
+                max_grad_norm=max_grad_norm,
+                warmup_ratio=warmup_ratio,
+                learning_rate=learning_rate,
+                lr_scheduler_type=lr_scheduler_type,
+                optim=optim,
+                weight_decay=weight_decay,
+                evaluation_strategy="epoch",
+                # eval_steps=0.2 / num_train_epochs - 0.001,
+                save_strategy="no",
+                # save_steps=0.5 / num_train_epochs - 0.001,
+                logging_dir=f"{results_dir}/logs/",
+                logging_strategy="steps",
+                logging_steps=0.1 / num_train_epochs,
+                report_to="none",
+                # load_best_model_at_end=True,
+                bf16=True,  # Enable mixed-precision training
+                bf16_full_eval=True,  # Enable mixed-precision evaluation
+                seed=seed,
             )
 
-        # Train the model
-        try:
+            # Prepare the dataset
+            self.prepare_dataset(dataset_size, dataset_split)
+
+            # Set the dropout rate and attention heads
+            config_1B.attention_dropout = attn_dropout
+            config_1B.num_attention_heads = attention_heads
+
+            # Initialize the trainer
+            trainer = SFTTrainer(
+                model_init=self.model_init,
+                args=self.training_args,
+                train_dataset=self.dataset_train,
+                eval_dataset=self.dataset_eval,
+                dataset_text_field="text",
+                packing=True,
+                max_seq_length=context_length,
+                tokenizer=tokenizer,
+                callbacks=[self],
+                # callbacks=[self, OptunaPruningCallback(trial, monitor="eval_loss")],
+            )
+
+            # Print the model size with suffix 'G' or 'M'
+            model_size = sum(p.numel() for p in trainer.model.parameters())
+            model_size = model_size / 1e9 if model_size > 1e9 else model_size / 1e6
+            model_size_suffix = "G" if model_size > 1e3 else "M"
+
+            dataset_train_size = len(self.dataset_train)
+            dataset_eval_size = len(self.dataset_eval)
+
+            # Print the hyperparameters
+            print("Hyperparameters:")
+            print(f"  Model size: {model_size:.2f}{model_size_suffix} parameters")
+            print(f"  Hidden layers: {hidden_layers}")
+            print(f"  Hidden size: {hidden_size}")
+            print(f"  Intermediate size: {intermediate_size}")
+            print(f"  Attention heads: {attention_heads}")
+            print(f"  Attention dropout: {attn_dropout}")
+            print(f"  Learning rate: {learning_rate}")
+            print(f"  Learning rate scheduler type: {lr_scheduler_type}")
+            print(f"  Epochs: {num_train_epochs}")
+            print(f"  Warmup ratio: {warmup_ratio}")
+            print(f"  Weight decay: {weight_decay}")
+            print(f"  Max gradient norm: {max_grad_norm}")
+            print(f"  Gradient accumulation steps: {gradient_accumulation_steps}")
+            print(f"  Per device train batch size: {per_device_train_batch_size}")
+            device_count = torch.cuda.device_count() if torch.cuda.is_available() else 1
+            print(f"  Device count: {device_count}")
+            print(f"  Effective batch size: {per_device_train_batch_size * gradient_accumulation_steps * device_count}")
+            print(f"  Dataset size: {dataset_size}")
+            print(f"  Training set size: {dataset_train_size}")
+            print(f"  Evaluation set size: {dataset_eval_size}")
+
+            # Save all the details to a JSON file in the results directory
+            with open(f"{results_dir}/details.json", "w") as f:
+                json.dump(
+                    {
+                        "model_size": f"{model_size:.2f}{model_size_suffix}",
+                        "learning_rate": learning_rate,
+                        "lr_scheduler_type": lr_scheduler_type,
+                        "per_device_train_batch_size": per_device_train_batch_size,
+                        "epochs": num_train_epochs,
+                        "warmup_ratio": warmup_ratio,
+                        "attention_dropout": attn_dropout,
+                        "gradient_accumulation_steps": gradient_accumulation_steps,
+                        "dataset_train_size": dataset_train_size,
+                        "dataset_eval_size": dataset_eval_size,
+                        # Model settings
+                        "hidden_layers": hidden_layers,
+                        "hidden_size": hidden_size,
+                        "intermediate_size": intermediate_size,
+                        "attention_heads": attention_heads,
+                        "attn_dropout": attn_dropout,
+                        "context_length": context_length,
+                        "template_model_name": template_model_name,
+                        # Dataset settings
+                        "dataset_name": dataset_name,
+                        "dataset_config": dataset_config,
+                        "dataset_path": dataset_path,
+                        "dataset_size": dataset_size,
+                        "dataset_split": dataset_split,
+                        "stride": stride,
+                        # Training settings
+                        "seed": seed,
+                        "lr_range": lr_range,
+                        "lr_scheduler_type": lr_scheduler_type,
+                        # "optim": optim,
+                    },
+                    f,
+                )
+
+            # Train the model
             trainer.train()
         except KeyboardInterrupt:
-            print("\n\nTraining interrupted by user")
+            print("\n\nTrial interrupted by user. Press Ctrl+C again to exit.")
             return self.best_loss
 
         # Save the model
