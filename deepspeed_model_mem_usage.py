2d8e36563b4741f2116cfd87da9db654cb5f601c
gronkomatic <sam@gillham.xyz>
2024-04-12 00:45:08 +1000

    Update deepspeed_model_mem_usage.py with new checkpoint path

--- deepspeed_model_mem_usage.py
+++ deepspeed_model_mem_usage.py
@@ -2,6 +2,5 @@ from transformers import AutoModel
 from deepspeed.runtime.zero.stage3 import estimate_zero3_model_states_mem_needs_all_live
 
 model = AutoModel.from_pretrained(
-    "/media/gronkomatic/Embiggen/ai-stuff/training-results/runs/run-20240330-025555/checkpoint-109698")
-estimate_zero3_model_states_mem_needs_all_live(model, num_gpus_per_node=2, num_nodes=1
-)
+    "/media/gronkomatic/Embiggen/ai-stuff/training-results/mistral-wiki/training-run-20240411-235819/checkpoint-205")
+estimate_zero3_model_states_mem_needs_all_live(model, num_gpus_per_node=2, num_nodes=1)
