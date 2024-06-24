447ba9c20bd9ca84f20fd1506fb62e9249613877
nomadicsynth <sam@gillham.xyz>
2024-06-24 20:09:53 +1000

    chore: Update requirements.txt and update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 source .venv/bin/activate
 pip install -U ninja wheel packaging
-pip install -U torch
 pip install -U -r requirements.txt
-pip uninstall flash-attn
+pip uninstall -y flash-attn
 pip install flash-attn --no-build-isolation
