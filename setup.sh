6ffe4d4537fb120fcf8c3a9c481dcfb5ad131c42
nomadicsynth <sam@gillham.xyz>
2024-07-10 14:25:27 +1000

    chore: Update requirements.txt and setup.sh to install transformers from forked repo

--- setup.sh
+++ setup.sh
@@ -3,5 +3,8 @@ source .venv/bin/activate
 pip install ninja wheel packaging
 pip install torch
 pip install -r requirements.txt
+pip uninstall -y transformers
+git clone https://github.com/nomadicsynth/transformers.git ./transformers-grokfast
+pip install -e ./transformers-grokfast
 pip install flash-attn --no-build-isolation
-wget https://raw.githubusercontent.com/ironjr/grokfast/main/grokfast.py
+# wget https://raw.githubusercontent.com/ironjr/grokfast/main/grokfast.py
